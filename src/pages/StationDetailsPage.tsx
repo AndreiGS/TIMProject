db96e55537aa5672bf16a023f4b8ac0cadc51543
Andrei Ghiurtu <andreibv96@yahoo.com>
2025-01-12 16:01:47 +0200

    leaflet marker issue on deployed

--- src/pages/StationDetailsPage.tsx
+++ src/pages/StationDetailsPage.tsx
@@ -5,6 +5,10 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { LatLng } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import "leaflet-defaulticon-compatibility";
+
 interface Station {
   name: string;
   timestamp: string;
